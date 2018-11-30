634aa8b4fca199b70ed2b218138fcc16019db6d4
JoyChenCN <354215459@qq.com>
2018-11-30 12:26:07 +0800

    fix bug & change style

--- EmojiArt_appDemo/EmojiArt/EmojiArtDocumentTableViewController.swift
+++ EmojiArt_appDemo/EmojiArt/EmojiArtDocumentTableViewController.swift
@@ -48,6 +48,13 @@ class EmojiArtDocumentTableViewController: UITableViewController {
         tableView.reloadData()
     }
     
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        if splitViewController?.preferredDisplayMode != .primaryOverlay {
+            splitViewController?.preferredDisplayMode = .primaryOverlay
+        }
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
--- EmojiArt_appDemo/EmojiArt/EmojiArtViewController.swift
+++ EmojiArt_appDemo/EmojiArt/EmojiArtViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class EmojiArtViewController: UIViewController, UIDropInteractionDelegate {
+class EmojiArtViewController: UIViewController, UIDropInteractionDelegate, UIScrollViewDelegate {
 
     @IBOutlet weak var dropZone: UIView!{
         didSet {
@@ -27,10 +27,41 @@ class EmojiArtViewController: UIViewController, UIDropInteractionDelegate {
     
     var imageFetcher: ImageFetcher!
     
+    var emojiArtView = EmojiArtView()
+    
+    @IBOutlet weak var scrollView: UIScrollView! {
+        didSet {
+            scrollView.minimumZoomScale = 0.1
+            scrollView.maximumZoomScale = 5.0
+            scrollView.delegate = self
+            scrollView.addSubview(emojiArtView)
+        }
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return emojiArtView
+    }
+    
+    var emojiArtBackgroundImage:UIImage? {
+        get {
+            return emojiArtView.backgroundImage
+        }
+        set {
+            scrollView?.zoomScale = 1.0
+            emojiArtView.backgroundImage = newValue
+            let size = newValue?.size ?? CGSize.zero
+            emojiArtView.frame = CGRect(origin: CGPoint.zero, size: size)
+            scrollView?.contentSize = size
+            if let dropZone = self.dropZone , size.width > 0 , size.height > 0 {
+                scrollView?.zoomScale = max(dropZone.bounds.size.width / size.width, dropZone.bounds.size.height / size.height)
+            }
+        }
+    }
+    
     func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
         imageFetcher = ImageFetcher() { (url, image) in
             DispatchQueue.main.async {
-                self.emojiArtView.backgroundImage = image
+                self.emojiArtBackgroundImage = image
             }
         }
         
@@ -47,7 +78,7 @@ class EmojiArtViewController: UIViewController, UIDropInteractionDelegate {
     }
     
     
-    @IBOutlet weak var emojiArtView: EmojiArtView!
+    
     
    
 
