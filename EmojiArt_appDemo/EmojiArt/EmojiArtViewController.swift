99b207bc524030ff94c1e1e24ef726c16d437b25
JoyChenCN <354215459@qq.com>
2018-12-02 15:43:08 +0800

    already finished & fix bugs

--- EmojiArt_appDemo/EmojiArt/EmojiArtViewController.swift
+++ EmojiArt_appDemo/EmojiArt/EmojiArtViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class EmojiArtViewController: UIViewController, UIDropInteractionDelegate, UIScrollViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout,UICollectionViewDelegate{
-
+class EmojiArtViewController: UIViewController, UIDropInteractionDelegate, UIScrollViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout,UICollectionViewDelegate, UICollectionViewDragDelegate, UICollectionViewDropDelegate{
+  
+    
     @IBOutlet weak var dropZone: UIView!{
         didSet {
             dropZone.addInteraction(UIDropInteraction(delegate: self))
@@ -31,6 +32,8 @@ class EmojiArtViewController: UIViewController, UIDropInteractionDelegate, UIScr
         didSet {
             emojiCollectionView.dataSource = self
             emojiCollectionView.delegate = self
+            emojiCollectionView.dragDelegate = self
+            emojiCollectionView.dropDelegate = self
         }
     }
     
@@ -39,8 +42,9 @@ class EmojiArtViewController: UIViewController, UIDropInteractionDelegate, UIScr
     }
     
     private var font: UIFont {
-        return UIFontMetrics(forTextStyle: .body).scaledFont(for: UIFont.preferredFont(forTextStyle: .body))
+        return UIFontMetrics(forTextStyle: .body).scaledFont(for: UIFont.preferredFont(forTextStyle: .body).withSize(64.0))
     }
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "EmojiCell", for: indexPath)
         if let emojiCell = cell as? EmojiCollectionViewCell {
@@ -50,6 +54,76 @@ class EmojiArtViewController: UIViewController, UIDropInteractionDelegate, UIScr
         return cell
     }
     
+    func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        session.localContext = collectionView
+        return dragItems(at: indexPath)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, itemsForAddingTo session: UIDragSession, at indexPath: IndexPath, point: CGPoint) -> [UIDragItem] {
+        return dragItems(at: indexPath)
+    }
+    
+    private func dragItems(at indexPath: IndexPath ) -> [UIDragItem] {
+        if let attributedString = (emojiCollectionView.cellForItem(at: indexPath) as? EmojiCollectionViewCell)?.label.attributedText{
+            let dragItem = UIDragItem(itemProvider: NSItemProvider(object: attributedString))
+            dragItem.localObject = attributedString
+            return [dragItem]
+        } else{
+            return []
+        }
+    }
+
+    
+    func collectionView(_ collectionView: UICollectionView, canHandle session: UIDropSession) -> Bool {
+        return session.canLoadObjects(ofClass: NSAttributedString.self)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, dropSessionDidUpdate session: UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UICollectionViewDropProposal {
+        let isSelf = (session.localDragSession?.localContext as? UICollectionView) == collectionView
+        return UICollectionViewDropProposal(operation: isSelf ? .move : .copy, intent: .insertAtDestinationIndexPath)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator) {
+        let destinationIndexPath = coordinator.destinationIndexPath ?? IndexPath(item: 0, section: 0)
+//        print("[1]emojis -> arrays = \(emojis.count)") //just for debugging
+        for item in coordinator.items {
+            if let sourceIndexPath = item.sourceIndexPath {
+                /*
+                 this code is use for  drag the emojis from collection view & drop it into the other situation.
+                */
+                if let attributedString = item.dragItem.localObject as? NSAttributedString {
+                        collectionView.performBatchUpdates({
+                            emojis.remove(at: sourceIndexPath.item)
+                            emojis.insert(attributedString.string, at: destinationIndexPath.item)
+                            collectionView.deleteItems(at: [sourceIndexPath])
+                            collectionView.insertItems(at: [destinationIndexPath])
+                        })
+                        coordinator.drop(item.dragItem, toItemAt: destinationIndexPath)
+                    }
+                } else {
+//                    print("[2]emojis -> arrays = \(emojis.count)") //for debugging cuz the code's shape is to like a rocket.
+                /*
+                  this code is used for drag other words from the website & make it to the place holder
+                 */
+                    let placeholderContext = coordinator.drop(
+                        item.dragItem,
+                        to: UICollectionViewDropPlaceholder(insertionIndexPath: destinationIndexPath, reuseIdentifier: "DropPlaceholderCell")
+                    )
+
+                    item.dragItem.itemProvider.loadObject(ofClass: NSAttributedString.self) { (provider, error) in
+                        DispatchQueue.main.sync {
+                            if let attributedString = provider as? NSAttributedString {
+                                placeholderContext.commitInsertion(dataSourceUpdates: { insertionIndexPath in
+                                    self.emojis.insert(attributedString.string, at: insertionIndexPath.item)
+                                })
+                            } else {
+                                placeholderContext.deletePlaceholder()
+                        }
+                    }
+                }
+            }
+        }
+    }
     
     func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
         return session.canLoadObjects(ofClass: NSURL.self) && session.canLoadObjects(ofClass: UIImage.self)
